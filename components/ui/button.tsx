ed9dab56c709ff8b3b8823e2655d9fa03e6e826a
vrindabindal12 <vrindabindal1212@gmail.com>
2025-02-12 20:56:21 +0530

    heeki

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,30 +1,34 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
 
+
+
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-zinc-300",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:-translate-y-0.5 transition-all duration-300",
   {
     variants: {
       variant: {
-        default:
-          "bg-zinc-900 text-zinc-50 shadow hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
-        destructive:
-          "bg-red-500 text-zinc-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
-        outline:
-          "border border-zinc-200 bg-white shadow-sm hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
-        secondary:
-          "bg-zinc-100 text-zinc-900 shadow-sm hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
-        ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
-        link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+        default: "bg-primary text-primary-foreground hover:opacity-70 hover:ring-4 hover:ring-primary/10",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        subtle: "border border-input bg-accent/20 hover:bg-white/10 hover:text-accent-foreground",
+        blue: "border border-input bg-blue-500 text-white hover:bg-blue-600",
+        secondary: "text-secondary-foreground btn-secondary font-normal",
+        ghost: "hover:bg-white/10 hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      white: "bg-white text-black hover:opacity-70",
+
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        xs: "h-7 px-2",
+        sm: "h-8 px-3",
+        lg: "h-10 px-8",
+        xl: "h-12 px-10",
+        icon: "h-8 w-8",
+        iconlg: "h-10 w-10",
       },
     },
     defaultVariants: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,21 +13,46 @@ const config: Config = {
   			'light-gray': '#d3d3d3',
   			'medium-gray': '#808080',
   			'dark-gray': '#333333',
-			'custom-bg': '#404040',
-			'custom-neon': '#78fff8',
-			'custom-white':'#d9d9d9',
+  			'custom-bg': '#404040',
+  			'custom-neon': '#78fff8',
+  			'custom-white': '#d9d9d9',
   			black: '#000000'
   		},
   		fontFamily: {
-  			inria: ['Inria Sans', 'sans-serif'],
-  			inter: ['Inter', 'sans-serif'],
-  			koho: ['Koho', 'sans-serif'],
-  			itim: ['Itim', 'cursive']
+  			inria: [
+  				'Inria Sans',
+  				'sans-serif'
+  			],
+  			inter: [
+  				'Inter',
+  				'sans-serif'
+  			],
+  			koho: [
+  				'Koho',
+  				'sans-serif'
+  			],
+  			itim: [
+  				'Itim',
+  				'cursive'
+  			]
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			orbit: 'orbit calc(var(--duration)*1s) linear infinite'
+  		},
+  		keyframes: {
+  			orbit: {
+  				'0%': {
+  					transform: 'rotate(calc(var(--angle) * 1deg)) translateY(calc(var(--radius) * 1px)) rotate(calc(var(--angle) * -1deg))'
+  				},
+  				'100%': {
+  					transform: 'rotate(calc(var(--angle) * 1deg + 360deg)) translateY(calc(var(--radius) * 1px)) rotate(calc((var(--angle) * -1deg) - 360deg))'
+  				}
+  			}
   		}
   	}
   },

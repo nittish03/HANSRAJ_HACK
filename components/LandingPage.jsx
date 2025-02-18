0aca757e8c2b3b2b96d12699cd203d24cf09ed9f
nittish03 <nittishbaboria123@gmail.com>
2025-02-18 22:30:48 +0530

    glow effect

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -143,6 +143,8 @@ const LandingPage = () => {
                     </Container>
                     <Container delay={0.3} className="relative">
                     <div className="relative flex items-center justify-center pt-20 w-full">
+                    <div className="absolute top-1/8 left-1/2 -z-10 bg-gradient-to-r from-sky-500 to-blue-600 w-1/2 lg:w-3/4 -translate-x-1/2 h-1/4 -translate-y-1/2 inset-0 blur-[4rem] lg:blur-[10rem] animate-image-glow"></div>
+                    <div className="hidden lg:block absolute -top-1/8 left-1/2 -z-20 bg-blue-600 w-1/4 -translate-x-1/2 h-1/4 -translate-y-1/2 inset-0 blur-[10rem] animate-image-glow"></div>
                 <div className="absolute top-1/2 left-1/2 -z-10 gradient w-3/4 -translate-x-1/2 h-3/4 -translate-y-1/2 inset-0 blur-[10rem]"></div>
                 <div className="-m-2 rounded-xl p-2 ring-1 ring-inset ring-foreground/20 lg:-m-4 lg:rounded-2xl bg-opacity-50 backdrop-blur-3xl text-center flex justify-center  items-center">
                   <Image

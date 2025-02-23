b9baa0df5d3f8b78afb4378b916b79459c356804
nittish03 <nittishbaboria123@gmail.com>
2025-02-23 14:29:27 +0530

    chatbot

--- app/api/mindcare/route.js
+++ app/api/mindcare/route.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
   try {
     const { message } = await req.json();

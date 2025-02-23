a55d815384e9e7a99cea1e553c933f51ca70817f
nittish03 <nittishbaboria123@gmail.com>
2025-02-23 15:21:53 +0530

    localhost

--- app/mindCare/page.jsx
+++ app/mindCare/page.jsx
@@ -10,7 +10,7 @@ const MindCareChat = () => {
   const generateResponse = async (query) => {
     setIsLoading(true);
     try {
-      const response = await fetch("/api/mindCare", {
+      const response = await fetch("/api/mindCare1", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
